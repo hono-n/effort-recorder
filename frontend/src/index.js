7f6da73cbc732f7f8fe91cedfedfa16273b8cb26
hono-n <honoka.nitta.biz@gmail.com>
2023-07-16 15:05:09 +0900

    changed to use @use instead of @import in scss files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'normalize.css';
-import './index.scss'
+import './assets/styles/_global-style.scss'
 
 import App from './App';
 
