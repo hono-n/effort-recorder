a0e6a3e1a03a2e99abb6c7f3dfe0e8fba76ed9cf
hono-n <honoka.nitta.biz@gmail.com>
2023-07-25 16:43:20 +0900

    implemented the components which compose the ProjectHistoryItem.js

---
+++ frontend/src/components/atoms/ProjectHistoryMemo/ProjectHistoryMemo.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import './ProjectHistoryMemo.scss';
+
+export default function ProjectHistoryMemo({ total, memo }) {
+
+  const formattedTotal = Math.round(total / 60000);
+
+  const [showTooltip, setShowTooltip] = useState(false);
+  const handleShowTooltip = (e) => {
+    e.stopPropagation();
+    setShowTooltip(true);
+  }
+  const handleHideTooltip = (e) => {
+    e.stopPropagation();
+    setShowTooltip(false);
+  }
+
+  return (
+    <div className="project-history-memo" onMouseOver={handleShowTooltip} onMouseLeave={handleHideTooltip}>
+      <div className="project-history-memo__total">
+        <p>{formattedTotal}分</p>
+      </div>
+      <div className="project-history-memo__memo-container">
+        <p className="project-history-memo__memo">{memo}</p>
+      </div>
+      {showTooltip && <MemoTooltip memo={memo} />}
+    </div>
+  )
+}
+
+
+export function MemoTooltip({ memo }) {
+  return (
+    <div className="memo-tooltip">
+      <div className="memo-tooltip__decoration"></div>
+      <div className="memo-tooltip__container">
+        <div className="memo-tooltip__header">MEMO</div>
+        <div className="memo-tooltip__content">
+          {memo}
+        </div>
+      </div>
+
+    </div>
+  )
+}
--- frontend/src/components/organisms/ProjectHistory/ProjectHistory.js
+++ frontend/src/components/organisms/ProjectHistory/ProjectHistory.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 import './ProjectHistory.scss';
+import ProjectHistoryItem from "../../molecules/ProjectHistoryItem/ProjectHistoryItem";
 
 
 export default function ProjectHistory() {
+
+  const d1 = new Date('2023-07-25T08:44:00').getTime();
+  const d2 = new Date('2023-07-25T12:02:00').getTime();
+  const d3 = new Date('2023-07-24T09:12:00').getTime();
+  const d4 = new Date('2023-07-24T09:18:36').getTime();
 
   return (
     <div className="project-history">
@@ -11,15 +17,43 @@ export default function ProjectHistory() {
       <div className="project-history__items-wrapper">
         <div className="project-history__items-container">
 
-            <div className="project-history__item"></div>
-            <div className="project-history__item"></div>
-            <div className="project-history__item"></div>
-            <div className="project-history__item"></div>
-            <div className="project-history__item"></div>
-            <div className="project-history__item"></div>
+          <ProjectHistoryItem
+            date='07/25（火）'
+            start_timestamp={d3}
+            end_timestamp={d4}
+            total={(d2 - d1) + (d4 - d3)}
+            memo='フレックスボックスのコツがなんとなく掴めてきたような気がするのである' />
+          <ProjectHistoryItem
+            start_timestamp={d3}
+            end_timestamp={d4} />
+          <ProjectHistoryItem
+            date='07/25（火）'
+            start_timestamp={d3}
+            end_timestamp={d4}
+            total={(d2 - d1) + (d4 - d3)}
+            memo='参考書一冊完了' />
+          <ProjectHistoryItem
 
+            start_timestamp={d3}
+            end_timestamp={d4}
+            total={(d2 - d1) + (d4 - d3)}
+            memo='参考書一冊完了' />
+          <ProjectHistoryItem
+            date='07/25（火）'
+            start_timestamp={d3}
+            end_timestamp={d4} />
         </div>
       </div>
     </div>
   )
 }
+
+const sampleData = [
+  {
+    id: 1,
+    start_timestamp: 1690252641901,
+    end_timestamp: 1690252902370,
+    cumulative_sum: 1200000,
+    memo: 'Reactのカスタムフックの使い方がわかった'
+  },
+];
