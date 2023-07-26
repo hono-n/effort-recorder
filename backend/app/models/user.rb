94265303db93382903725e4d41fdea96eec77e38
hono-n <honoka.nitta.biz@gmail.com>
2023-07-26 13:27:20 +0900

    [Rails] added the validation on user_name column for user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :user_name, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true, length: { maximum: 16 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
