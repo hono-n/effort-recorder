a10e1d09c0b0214b352a7b7295cebd7968867754
hono-n <honoka.nitta.biz@gmail.com>
2023-07-27 10:48:11 +0900

    extracted the logic of creating a new project into custom hook

--- frontend/src/components/organisms/ProjectList/ProjectList.js
+++ frontend/src/components/organisms/ProjectList/ProjectList.js
@@ -1,45 +1,43 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import React, { useEffect } from "react";
 
 import ProjectListItem from "../../atoms/ProjectListItem/ProjectListItem";
 import LinkButton from "../../molecules/LinkButton/LinkButton";
 import Modal from "../../molecules/Modal/Modal";
 import InputBoxWithCount from "../../molecules/InputBox/InputBoxWithCount";
-
-import { useProjectList } from "../../../hooks/ProjectList.hook";
-
-import { useAuth } from "../../../contexts/AuthContext";
-import { useFlashMessageContext } from "../../../contexts/FlashMessageContext";
-import { useUpdateFormValue } from "../../../hooks/FormHandler.hook";
-// import { useProjectContext } from "../../../contexts/ProjectContext";
-
 import Button from "../../molecules/Button/Button";
 
+import { useProjectList } from "../../../hooks/ProjectList.hook";
 import './ProjectList.scss';
 
 
 export default function ProjectList() {
-
   const {
     projects,
-    setProjects,
     isLoading,
+    showModal,
+    setShowModal,
     handleLoad,
-    handleFormAction
+    handleFormAction,
+    handleInputValue,
   } = useProjectList();
 
-  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     handleLoad();
-  }, []);
+  }, [projects]);
 
   return (
     <div className="project-list">
       {showModal &&
         <Modal
           title='プロジェクトの追加'
-          children={<ModalContent closeModal={() => setShowModal(false)} />}
+          children={
+            <ModalContent
+              closeModal={() => setShowModal(false)}
+              handleFormAction={handleFormAction}
+              handleInputValue={handleInputValue}
+            />
+          }
           handleClick={() => setShowModal(false)} />
       }
       <div className="project-list__overview">
@@ -62,59 +60,21 @@ export default function ProjectList() {
   )
 }
 
-function ModalContent({ closeModal }) {
-
-  const { user } = useAuth();
-  const { setShowFlashMessage, setFlashMessage } = useFlashMessageContext();
-
-  const [formData, setFormData] = useState({ projectName: '' });
-  const updateFormData = useUpdateFormValue({
-    formData: formData,
-    setFormData: setFormData,
-  });
-
-  function handleInputValue(fieldName, inputValue) {
-    updateFormData(fieldName, inputValue);
-  }
-
-  const requestUrl = `http://localhost:3001/api/users/${user.id}/projects`;
-
-  const handleCreateProject = (event) => {
-    axios.post(requestUrl,
-      {
-        project: {
-          user_id: user.id,
-          name: formData.projectName,
-        }
-      },
-      { withCredentials: true }
-    ).then(response => {
-      if (response.data.status === 'created') {
-        console.log(response.data);
-        closeModal();
-        setShowFlashMessage(true);
-        setFlashMessage({ type: 'success', message: 'プロジェクトを作成しました' });
-      }
-      else {
-        console.log(response.data);
-        setShowFlashMessage(true);
-        // Rails側で Userモデルのレコード追加に失敗した場合
-        setFlashMessage({ type: 'error', message: 'プロジェクトの作成に失敗しました' });
-      }
-    }).catch(error => {
-      // Rails側が応答できなかった場合（サーバーが落ちているなど）
-      console.log('【React】Railsで何か問題があるようです', error);
-      setShowFlashMessage(true);
-      setFlashMessage({ type: 'error', message: '予期せぬエラーが発生しました。再度お試しください' });
-    });
-    event.preventDefault();
-  }
+function ModalContent({ handleFormAction, handleInputValue }) {
 
   return (
-    <form onSubmit={handleCreateProject}>
+    <form onSubmit={handleFormAction}>
       <div className="modal-content">
-        <InputBoxWithCount label='プロジェクト名' placeholder='プロジェクト名を入力' max_char={15} handleInputValue={{ callback: handleInputValue, fieldName: 'projectName' }} />
-        <Button className='modal-content__button' type='submit' label='作成' />
+        <InputBoxWithCount
+          label='プロジェクト名'
+          placeholder='プロジェクト名を入力'
+          max_char={15}
+          handleInputValue={{ callback: handleInputValue, fieldName: 'projectName' }}
+        />
+        <Button
+          className='modal-content__button'
+          type='submit'
+          label='作成' />
       </div>
     </form>
   )
--- frontend/src/hooks/ProjectList.hook.js
+++ frontend/src/hooks/ProjectList.hook.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios'
 
 
@@ -15,6 +15,15 @@ export function useProjectList() {
   const { projects, setProjects } = useProjectContext();
 
   const [isLoading, setIsLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({ projectName: '' });
+  const updateFormData = useUpdateFormValue({
+    formData: formData,
+    setFormData: setFormData,
+  });
+  function handleInputValue(fieldName, inputValue) {
+    updateFormData(fieldName, inputValue);
+  }
 
   const handleLoad = async () => {
     const requestUrl = `http://localhost:3001/api/users/${user.id}/projects`;
@@ -33,21 +42,47 @@ export function useProjectList() {
     setIsLoading(false);
   };
 
+  const handleCreateProject = (event) => {
+    const requestUrl = `http://localhost:3001/api/users/${user.id}/projects`;
+    axios.post(requestUrl,
+      {
+        project: {
+          user_id: user.id,
+          name: formData.projectName,
+        }
+      },
+      { withCredentials: true }
+    ).then(response => {
+      if (response.data.status === 'created') {
+        setShowModal(false);
+        setShowFlashMessage(true);
+        setFlashMessage({ type: 'success', message: 'プロジェクトを作成しました' });
+      }
+      else {
+        setShowFlashMessage(true);
+        // Rails側で Userモデルのレコード追加に失敗した場合
+        setFlashMessage({ type: 'error', message: 'プロジェクトの作成に失敗しました' });
+      }
+    }).catch(error => {
+      // Rails側が応答できなかった場合（サーバーが落ちているなど）
+      console.log('【React】Railsで何か問題があるようです', error);
+      setShowFlashMessage(true);
+      setFlashMessage({ type: 'error', message: '予期せぬエラーが発生しました。再度お試しください' });
+    });
+    event.preventDefault();
+  }
+
 
   const projectList = {
-    // formData: formData,
-    // updateFormValue: useUpdateFormValue({
-    //   formData: formData,
-    //   setFormData: setFormData,
-    // }),
+    formData: formData,
     isLoading,
     projects,
-    // showModal,
-    // setShowModal,
+    showModal,
+    setShowModal,
     handleLoad: handleLoad,
-    // handleFormAction: handleCreateProject,
+    handleFormAction: handleCreateProject,
+    handleInputValue: handleInputValue,
   };
-
   return projectList;
 }
 
