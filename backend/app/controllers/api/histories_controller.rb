9c0df39af761b039507a9a72c77841202cd92ad6
hono-n <honoka.nitta.biz@gmail.com>
2023-07-28 13:21:14 +0900

    (1) added HistoriesController (2) configured routing of HistoriesContoroller (3) implemented index action

---
+++ backend/app/controllers/api/histories_controller.rb
@@ -0,0 +1,24 @@
+class Api::HistoriesController < ApplicationController
+  def index
+    # ネストされたリソースにおける取得の仕方：params[:リソース名の単数系_id]
+    @user = User.find(params[:user_id])
+    @projects = @user.projects.find(params[:project_id])
+    @histories = @projects.histories
+    render json: { status: :ok, projects: @histories }
+  end
+
+  # def create
+  #   @user = User.find(params[:user_id])
+  #   @project = @user.projects.new(project_params)
+
+  #   if @project.save
+  #     render json: { status: :created, project: @project }
+  #   else
+  #     render json: { status: 500, errors: @project.errors }
+  #   end
+  # end
+
+  def project_params
+    params.require(:project).permit(:name, :user_id)
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users, only: %i[create] do
-      resources :projects, only: %i[index show create destroy]
+      resources :projects, only: %i[index create] do
+        resources :histories, only: %i[index create]
+      end
     end
     resource :session, only: %i[show create destroy]
   end
